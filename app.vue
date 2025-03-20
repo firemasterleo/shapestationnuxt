80ef7a6f20d11ae5c4c97404aaefd460dd6416ef
firemasterleo <firemasterleo@outlook.com>
2025-03-20 10:31:36 -0400

    commit

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { useHead } from '#imports'
+
+useHead({
+  link: [
+    {
+      rel: 'preload',
+      href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
+      as: 'style'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+    }
+  ]
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
