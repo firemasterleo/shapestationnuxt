48d66e002c4155b5708aa5fe05db55a03dc1f1f4
firemasterleo <firemasterleo@outlook.com>
2025-03-24 05:16:48 -0400

    commit

--- components/Header.vue
+++ components/Header.vue
@@ -54,23 +54,17 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
 
               </div>
-                  <svg class="user" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path></svg>
+                  <svg class="user-svg" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path></svg>
             </div>
-                  <!-- User Dashboard Menu -->
-      <div v-if="showMenu" class="dropdown-menu">
-        <NuxtLink to="/profile">Profile</NuxtLink>
-        <NuxtLink to="/orders">My Orders</NuxtLink>
-        <button @click="userStore.logout()">Logout</button>
-      </div>
             <NuxtLink to="/cart" >
               <div class="cart">
-                <div class="svg">
+
                   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M104,216a16,16,0,1,1-16-16A16,16,0,0,1,104,216Zm88-16a16,16,0,1,0,16,16A16,16,0,0,0,192,200ZM239.71,74.14l-25.64,92.28A24.06,24.06,0,0,1,191,184H92.16A24.06,24.06,0,0,1,69,166.42L33.92,40H16a8,8,0,0,1,0-16H40a8,8,0,0,1,7.71,5.86L57.19,64H232a8,8,0,0,1,7.71,10.14ZM221.47,80H61.64l22.81,82.14A8,8,0,0,0,92.16,168H191a8,8,0,0,0,7.71-5.86Z"></path></svg>
                   <h1>
                         {{ cartStore.totalItems }}
         
                       </h1>
-                </div>
+     
 
               </div>
               </NuxtLink>
@@ -268,7 +262,7 @@
                 display: flex;
                 justify-content: space-between;
                 height: 3.2rem;
-                height: fit-content;
+                // height: fit-content;
                 background-color: rgb(231, 229, 227); /* Solid black background */
                 // background-color: rgb(121, 140, 97); /* Solid black background */
 
@@ -372,12 +366,7 @@
                 .right {
                   display: flex;
                   gap: 1rem;
-                  // width: 10.5rem;
-                  // border: solid green;
-                  // position: fixed;
-                // z-index: 25;
-                // mix-blend-mode: difference;
-                right: 0.5rem;
+
                   .user {
                     display: flex;
                     align-items: center;
@@ -387,6 +376,7 @@
                     .signin {
                     display: flex;
                     align-items: center;
+                    // border: solid red;
 
                       p {
                         color: $textcolorblack;
@@ -399,52 +389,42 @@
                       }
 
                     }
-                    .user {
+                    .user-svg {
                       fill: $textcolorblack;
-                      width: 35px;
-                      height: 35px;
+                      width: 30px;
+                      height: 30px;
                     }
                   }
 
                   .cart {
                     position: relative;
-                // z-index: 25;
-                // mix-blend-mode: difference;
-                // right: 0.5rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    height: 100%;
+                    padding-right: 1rem;
+
                 
                 // border: solid green;
-                padding-right: 1rem;
-                    
-                    
-                    .svg {
-                      display: flex;
-                      align-items: center;
-                      padding-top: 0.2rem;
-                      // border: solid green;
-                      // width:3.5rem;
-                      overflow: hidden;
-                      position: relative;
-               
 
-                      
                       svg {
                         fill: $textcolorblack;
-                        width: 40px;
-                        height: 40px;
+                        width: 35px;
+                        height: 35px;
                         
                       }
                       h1 {
                         
                         color: $textcolorblack;
-                        font-size: 14px;
+                        font-size: 12px;
                         position: absolute;
                       top: 50%;
                       left: 50%;
-                      transform: translate(-30%, -40%);
+                      transform: translate(-120%, -50%);
   
                       }
   
-                    }
+
                   
                 }
                 }
