5bda2ffd3d4041341f8473a390b794366006f7bc
firemasterleo <firemasterleo@outlook.com>
2025-03-30 08:44:15 -0400

    Update router.options.ts

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,12 +2,16 @@ import type { RouterConfig } from '@nuxt/schema';
 
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      // Restore scroll position on back/forward navigation
-      return savedPosition;
-    } else {
-      // Instantly reset scroll to top for new pages (without causing a flicker)
-      return { top: 0, left: 0 };
-    }
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (savedPosition) {
+          // Restore scroll position on back/forward navigation
+          resolve(savedPosition);
+        } else {
+          // Smooth scroll to top in 0.3s
+          resolve({ top: 0, left: 0, behavior: 'smooth' });
+        }
+      }, 10); // Tiny delay to prevent flicker
+    });
   }
 };
