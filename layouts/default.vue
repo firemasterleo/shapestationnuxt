52a67721aabe887f7dedf0a3c8143dac206d3529
firemasterleo <firemasterleo@outlook.com>
2025-03-30 19:23:53 -0400

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,10 +11,9 @@
     <div class="sidepanel-overlay" :class="{ 'is-expanded': isMenuOpen }" @click="closeMenu"></div>
 
     <!-- Main Content Area -->
-<main>
+ 
   <slot />
 
-</main>
 
 
     <!-- <Footer /> -->
